e7c46d773841fc601b6a7ccea870c760a74dd19b
Otto <gramazo123@mesoamericana.edu.gt>
2015-11-15 14:37:29 -0600

    Creacion de vistas para mostrar una banda y sus canciones.

--- letras/urls.py
+++ letras/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.lista_letras),
+    url(r'^banda/(?P<pk>[0-9]+)/$', views.detalles_banda),
 ]
 
--- letras/views.py
+++ letras/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Banda
 
@@ -6,3 +7,6 @@ def lista_letras(request):
     posts = Banda.objects.all()
     return render(request, 'letras/lista_letras.html', {'posts': posts})
 
+def detalles_banda(request, pk):
+    post = get_object_or_404(Banda, pk=pk)
+    return render(request, 'letras/detalles_banda.html', {'post': post})
